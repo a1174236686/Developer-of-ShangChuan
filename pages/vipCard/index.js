44adc663d14e253f29bbb06c1d12f64128745c21
qa3523697 <1516589790@qq.com>
2020-05-01 23:49:03 +0800

    0501+3

--- pages/vipCard/index.js
+++ pages/vipCard/index.js
@@ -1,12 +1,12 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const serverUrl = app.globalData.serverUrl
 Page({
   data: {
     number:'NO.000002',
     grade:'5',
-  
+    serverUrl: serverUrl
   
   },
  
